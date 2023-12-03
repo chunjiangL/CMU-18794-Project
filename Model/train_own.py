5f3648cdd578ed97bea7ef3abb59657874bad632
Tonychen050400 <61198124+Tonychen050400@users.noreply.github.com>
2023-12-02 22:23:04 -0500

    Add files via upload
    Latest CNN model in 12/2/2023

--- Model/train_own.py
+++ Model/train_own.py
@@ -5,14 +5,12 @@ import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
 import os
+import torch.nn.functional as F
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 # Assuming the histogram data has shape (num_samples, channels, height, width)
 # If not, you will need to reshape the data accordingly.
 
-class ConvBlock(nn.Module):
-    """
-    A 2D convolution, followed by batchnorm, a ReLU activation, and dropout.
-    """
+class EnhancedConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, dropout):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=kernel_size//2)
@@ -26,29 +24,37 @@ class ConvBlock(nn.Module):
         x = self.relu(x)
         return self.dropout(x)
 
-
-class ConvNet(nn.Module):
-    """
-    A CNN for processing histogram data.
-    """
+class EnhancedConvNet(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
-        self.layer1 = ConvBlock(in_channels, 64, kernel_size=3, stride=1, dropout=0.5)
-        self.layer2 = ConvBlock(64, 128, kernel_size=3, stride=1, dropout=0.5)
-        # Add more layers as needed
+        # Add more layers and increase complexity
+        self.layer1 = EnhancedConvBlock(in_channels, 64, kernel_size=3, stride=1, dropout=0.3)
+        self.layer2 = EnhancedConvBlock(64, 128, kernel_size=3, stride=1, dropout=0.3)
+        self.layer3 = EnhancedConvBlock(128, 256, kernel_size=3, stride=1, dropout=0.4)
+        self.layer4 = EnhancedConvBlock(256, 512, kernel_size=3, stride=1, dropout=0.4)
+
+        # Adaptive pooling layer remains the same
         self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        # Flatten layer
         self.flatten = nn.Flatten()
-        self.fc = nn.Linear(128, num_classes)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
         x = self.adaptive_pool(x)
         x = self.flatten(x)
-        x = self.fc(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
-def regression_accuracy(output, target, threshold=0.05):
+def regression_accuracy(output, target, threshold=0.15):
     """
     Calculate the 'accuracy' for a regression task. A prediction is considered
     correct if it's within a specified threshold of the actual value.
@@ -65,15 +71,20 @@ def regression_accuracy(output, target, threshold=0.05):
     accuracy = torch.mean(correct.float()) * 100
     return accuracy.item()
 
-def l1_l2_loss(output, target, l1_weight=0.5):
-    mse_loss = nn.MSELoss()(output, target)
-    l1_loss = nn.L1Loss()(output, target)
-    return l1_weight * l1_loss + (1 - l1_weight) * mse_loss
+def l1_l2_loss(pred, true, l1_weight):
+    loss = F.mse_loss(pred, true)
+
+    if l1_weight > 0:
+        l1 = F.l1_loss(pred, true)
+        loss += l1
+
+    return loss
 
 # Define training function
 def train_model(model, train_loader, val_loader, epochs, learning_rate, weight_decay, patience, device):
     #optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-    optimizer =torch.optim.SGD(model.parameters(), lr=learning_rate,momentum=0.99,weight_decay=weight_decay)
+    optimizer =torch.optim.SGD(model.parameters(), lr=learning_rate,momentum=0.9,weight_decay=weight_decay)
+    #scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
     best_val_loss = np.inf
     best_val_accuracy = 0
     train_losses, val_losses, train_accuracies, val_accuracies = [], [], [], []
@@ -87,7 +98,7 @@ def train_model(model, train_loader, val_loader, epochs, learning_rate, weight_d
             inputs, targets = inputs.to(device), targets.to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = l1_l2_loss(outputs, targets)
+            loss = l1_l2_loss(outputs, targets, l1_weight = 0.5)
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
@@ -103,12 +114,15 @@ def train_model(model, train_loader, val_loader, epochs, learning_rate, weight_d
             for inputs, targets in val_loader:
                 inputs, targets = inputs.to(device), targets.to(device)
                 outputs = model(inputs)
-                val_loss = l1_l2_loss(outputs, targets)
+                val_loss = l1_l2_loss(outputs, targets,l1_weight = 0.5)
                 total_val_loss += val_loss.item()
                 total_val_accuracy += regression_accuracy(outputs, targets)
 
         avg_val_loss = total_val_loss / len(val_loader)
         avg_val_accuracy = total_val_accuracy / len(val_loader)
+
+        # Scheduler step    
+        #scheduler.step(avg_val_loss)
 
         # Update best validation accuracy and save model state
         if avg_val_loss < best_val_loss:
@@ -164,11 +178,17 @@ yields = data['output_yield']
 years = data['output_year']
 
 # Normalize images
-images = (images - images.mean(axis=(0, 2, 3), keepdims=True))
+# images = (images - images.mean(axis=(0, 2, 3), keepdims=True))
+# Normalize images
+# Calculate mean and standard deviation for normalization
+mean = images.mean(axis=(0, 2, 3), keepdims=True)
+std = images.std(axis=(0, 2, 3), keepdims=True)
+# Apply normalization: (X - mean) / std
+images_normalized = (images - mean) / std
 
 # Split the data into training and validation sets
-train_indices = (years >= 2018) & (years <= 2021)
-val_indices = years == 2022
+train_indices = (years >= 2001) & (years <= 2021)
+val_indices = years ==2022
 train_images, val_images = images[train_indices], images[val_indices]
 train_yields, val_yields = yields[train_indices], yields[val_indices]
 
@@ -178,15 +198,17 @@ val_images_tensor = torch.tensor(val_images, dtype=torch.float32)
 train_yields_tensor = torch.tensor(train_yields, dtype=torch.float32).view(-1, 1)
 val_yields_tensor = torch.tensor(val_yields, dtype=torch.float32).view(-1, 1)
 
+
 # Create DataLoaders
 train_dataset = TensorDataset(train_images_tensor, train_yields_tensor)
 val_dataset = TensorDataset(val_images_tensor, val_yields_tensor)
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=32)
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=16)
 
 # Initialize the model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = ConvNet(in_channels=12, num_classes=1)  # Adjust in_channels according to your data
+# Initialize the enhanced model
+model = EnhancedConvNet(in_channels=12, num_classes=1)
 model.to(device)
 
 # Train the model
@@ -194,9 +216,10 @@ train_losses, val_losses, train_accuracies, val_accuracies = train_model(
     model=model,
     train_loader=train_loader,
     val_loader=val_loader,
-    epochs=500,  # Set the number of epochs
-    learning_rate=1e-3,  # Set the learning rate
-    weight_decay=5e-4,  # Set the weight decay
-    patience=50,  # Set the patience for early stopping
+    epochs=1000,  # Adjust as needed
+    learning_rate=0.0001,  # Adjust as needed
+    weight_decay=5e-4,  # Adjust as needed
+    patience=20,  # Adjust as needed
     device=device
 )
+print(model)
